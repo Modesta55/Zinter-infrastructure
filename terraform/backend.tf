e1cb9a612578ea1cb3f4020da79aa838ae4c6f58
Modesta55 <uchechimodesta97@gmail.com>
2025-05-30 09:55:37 +0100

    first push

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,10 @@
-terraform {
-  backend "remote" {
-    organization = "tf-demo-gcp-modesta"
+terraform { 
+  cloud { 
+    
+    organization = "tf-demo-gcp-modesta" 
 
-    workspaces {
-      name = "zinter-infra-workspace"
-    }
-  }
+    workspaces { 
+      name = "zinter-infra-workspace" 
+    } 
+  } 
 }
